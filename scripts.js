79cfdc2d26f43ddffcc642dfbb883d22ea986e18
danna9075 <damna038@gmail.com>
2024-06-15 09:28:33 -0500

    nuevos ajustes a la calculadora

--- scripts.js
+++ scripts.js
@@ -1,33 +1,54 @@
 function sumar() {
-    var num1 = parseInt(document.getElementById('num1').value);
-    var num2 = parseInt(document.getElementById('num2').value);
-    var resultado = num1 + num2;
-    document.getElementById('resultado').innerText = resultado;
-  }
-  
-  function restar() {
-    var num1 = parseInt(document.getElementById('num1').value);
-    var num2 = parseInt(document.getElementById('num2').value);
-    var resultado = num1 - num2;
-    document.getElementById('resultado').innerText = resultado;
-  }
-  
-  function multiplicar() {
-    var num1 = parseInt(document.getElementById('num1').value);
-    var num2 = parseInt(document.getElementById('num2').value);
-    var resultado = num1 * num2;
-    document.getElementById('resultado').innerText = resultado;
-  }
-  
-  function dividir() {
-    var num1 = parseInt(document.getElementById('num1').value);
-    var num2 = parseInt(document.getElementById('num2').value);
-    var resultado = num1 / num2;
-    document.getElementById('resultado').innerText = resultado;
-  }
-  
-  function cleaner(){
-    document.getElementById('num1').value="";
-    document.getElementById('num2').value="";
-    document.getElementById('resultado').innerHTML="";
-  }
+  var num1 = parseFloat(document.getElementById('num1').value);
+  var num2 = parseFloat(document.getElementById('num2').value);
+  var resultado = num1 + num2;
+  document.getElementById('resultado').innerText = resultado;
+}
+
+function restar() {
+  var num1 = parseFloat(document.getElementById('num1').value);
+  var num2 = parseFloat(document.getElementById('num2').value);
+  var resultado = num1 - num2;
+  document.getElementById('resultado').innerText = resultado;
+}
+
+function multiplicar() {
+  var num1 = parseFloat(document.getElementById('num1').value);
+  var num2 = parseFloat(document.getElementById('num2').value);
+  var resultado = num1 * num2;
+  document.getElementById('resultado').innerText = resultado;
+}
+
+function dividir() {
+  var num1 = parseFloat(document.getElementById('num1').value);
+  var num2 = parseFloat(document.getElementById('num2').value);
+  var resultado = num1 / num2;
+  document.getElementById('resultado').innerText = resultado;
+}
+
+function potencia() {
+  var  num1 = parseFloat(document.getElementById('num1').value);
+  var num2 = parseFloat(document.getElementById('num2').value);
+  var resultado = math.pow(nume1,nume2);
+  document.getElementById('resultado').innerText = resultado;
+}
+
+
+function raiz() {
+  var num1 = parseFloat(document.getElementById('num1').value);
+  var resultado = Math.sqrt(num1);
+  document.getElementById('resultado').innerText = resultado;
+}
+
+function modulo() {
+  var num1 = parseFloat(document.getElementById('num1').value);
+  var num2 = parseFloat(document.getElementById('num2').value);
+  var resultado = num1 % num2;
+  document.getElementById('resultado').innerText = resultado;
+}
+
+function cleaner(){
+  document.getElementById('num1').value="";
+  document.getElementById('num2').value="";
+  document.getElementById('resultado').innerHTML="";
+}
